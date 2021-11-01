19e88313f019eae2aba9d3d76907ef16d63d4bbd
RayanKhyare <71662425+RayanKhyare@users.noreply.github.com>
2021-11-01 12:48:19 +0100

    Made sexygraphs for S4

---
+++ S4/sexygraphs/src/index.js
@@ -0,0 +1,47 @@
+import Chart from 'chart.js/auto';
+
+
+const data = {
+    labels: ['DEV IV', 'Frontend', 'Backend', 'Full Projects IV', 'Motion Graphics', 'Design IV'],
+    datasets: [{
+        axis: 'y',
+        label: 'Rayan Khyare',
+        data: [9, 9, 7, 5, 8, 4],
+        fill: false,
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)'
+        ],
+        borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)'
+        ],
+        tension: 0.1,
+        borderWidth: 1,
+
+    }]
+};
+
+const ctx = document.getElementById('myChart');
+const myChart = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: {
+        indexAxis: 'x',
+        scales: {
+            x: {
+                beginAtZero: true
+            }
+        }
+    }
+});
