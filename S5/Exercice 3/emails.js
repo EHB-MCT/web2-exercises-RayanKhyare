3dc154adb0ac71981c24ed20b1e75640325d4d8e
RayanKhyare <71662425+RayanKhyare@users.noreply.github.com>
2021-11-16 17:43:05 +0100

    Made exercice 3 of S5

--- S5/Exercice 3/emails.js
+++ S5/Exercice 3/emails.js
@@ -1,1 +1,25 @@
 import axios from 'axios';
+import * as fs from 'fs/promises';
+
+
+try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/comments');
+
+    let emails = {
+        count: 0,
+        list: []
+    }
+
+    for (let comment of response.data) {
+        //add to file list
+        emails.list.push(comment.email);
+
+        emails.count++;
+    }
+
+
+    await fs.appendFile('emails.json', JSON.stringify(emails));
+    console.log(response.data[0])
+} catch (error) {
+    console.error(error);
+}
